b18138aba186a024ad44fcbdc63c7dd91fa6b37e
Tiago <tiagodesousacosta2024@gmail.com>
2025-11-30 20:46:21 -0300

    comitv5

--- app/google_service.py
+++ app/google_service.py
@@ -4,12 +4,20 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 def conectar():
+    # Lê a variável de ambiente que contém o JSON inteiro
     cred_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
     if not cred_json:
         raise Exception("Variável GOOGLE_APPLICATION_CREDENTIALS não encontrada!")
 
+    # Converte string → dicionário
     info = json.loads(cred_json)
-    creds = Credentials.from_service_account_info(info)
+
+    # Autentica usando o JSON
+    creds = Credentials.from_service_account_info(
+        info,
+        scopes=["https://www.googleapis.com/auth/spreadsheets"]
+    )
+
     client = gspread.authorize(creds)
     return client
