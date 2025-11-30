1d635fb098934332c4548bfde7cf6a8e95f8ada5
Tiago <tiagodesousacosta2024@gmail.com>
2025-11-30 20:43:41 -0300

    comitv4

--- app/google_service.py
+++ app/google_service.py
@@ -1,11 +1,15 @@
+import os
+import json
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 def conectar():
-    scope = ["https://www.googleapis.com/auth/spreadsheets"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials/service_account.json",
-        scope
-    )
+    cred_json = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+
+    if not cred_json:
+        raise Exception("Variável GOOGLE_APPLICATION_CREDENTIALS não encontrada!")
+
+    info = json.loads(cred_json)
+    creds = Credentials.from_service_account_info(info)
     client = gspread.authorize(creds)
     return client
